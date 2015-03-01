bf3bb5db207f70df1b2f524fcc9f7bbca106f1f8
Andrew Shaffer <darawk@gmail.com>
2015-03-01 14:58:22 -0800

    modularize tests

--- index.js
+++ index.js
@@ -15,7 +15,39 @@ exports.model = model
   .use(require('required'))
   .use(require('defaults'))
   .use(require('discriminators'));
+  
 
 // These can be overriden by simply calling .type() on model
 // again, so its ok to add them globally
 require('types')(exports.model);
+
+
+// This exists primarily for tests to easily swap out
+// a module with their local copy.  This code is not
+// compatible with browserify, so it is not recommended
+// to be used unless you want to sacrifice client-side
+// compatibility
+var plugins = [
+  'getter-setter', 
+  'cast', 
+  'methods', 
+  'attrs', 
+  'validation', 
+  'required', 
+  'defaults',
+  'discriminators'
+];
+
+exports.plugins = plugins;
+
+exports.bootstrap = function(replace) {
+  replace = replace || {};
+
+  var strapped = model;
+  plugins.forEach(function(plugin) {
+    strapped = strapped.use(replace[plugin] || require(plugin));
+  });
+
+  replace.types && replace.types(strapped);
+  return strapped;
+};
